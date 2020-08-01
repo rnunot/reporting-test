470b797ef6df47960a62758b56cd47ef6664bba0
Ricardo Tavares <rnunot@gmail.com>
2020-08-01 21:29:04 +0100

    Apply new linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "@vue/airbnb",
-    "eslint:recommended",
-    "@vue/prettier",
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    'eslint:recommended',
+    '@vue/prettier',
   ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
-import { preset } from "vue-cli-plugin-vuetify-preset-rally/preset";
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset';
 
 Vue.use(Vuetify);
 
